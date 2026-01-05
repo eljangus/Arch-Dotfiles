8b4727f4651d1c0bd9d10aff80cfa6165d6810d4
eljangus <eljangus@gmail.com>
2026-01-05 20:50:55 +0100

    light mode script fix

--- .config/RofiScripts/Dark-Light-Mode/Hell/hell.sh
+++ .config/RofiScripts/Dark-Light-Mode/Hell/hell.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
+dconf write /org/gnome/desktop/interface/color-scheme "'prefer-light'"
 ln -sfn ~/.config/RofiScripts/Walls-light/wall.sh ~/.config/RofiScripts/WallpaperChanger/wall.sh
 ln -sfn ~/.config/RofiScripts/Walls-light/wallrandom.sh ~/.config/RofiScripts/WallpaperChanger/wallrandom.sh
 matugen image ~/.config/RofiScripts/Walls-light/Wall -m light -t scheme-tonal-spot
