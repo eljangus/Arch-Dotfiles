8f6a69207683baebeb18a27562a74045e5e62ee6
eljangus <eljangus@gmail.com>
2026-01-28 22:35:13 +0100

    fixed System Settings rofi script

--- .config/RofiScripts/SystemSettings/hyprland.sh
+++ .config/RofiScripts/SystemSettings/hyprland.sh
@@ -3,17 +3,17 @@
 chosen=$(printf "󱓞 Autostart\n󰪫 Umgebungsvariablen\n󰍽 Eingabe\n󰌌 Kurzbefehle\n Aussehen und Handhabung\n󰍹 Bildschirme\n Berechtigungen\n Programme\n Plugins\n Fenster und Arbeitsflächen\n󰥛 Animationen\n󰘇 Dekorationen\n" | rofi -dmenu -i -config '~/.config/RofiScripts/SystemSettings/S.rasi')
 
 case "$chosen" in
-   "󱓞 Autostart") codium ~/.config/hypr/hyprconfigs/hyprautostart.conf ;;
-   "󰪫 Umgebungsvariablen") codium ~/.config/hypr/hyprconfigs/hyprenvironment.conf ;;
-   "󰍽 Eingabe") codium ~/.config/hypr/hyprconfigs/hyprinput.conf ;;
-   "󰌌 Kurzbefehle") codium ~/.config/hypr/hyprconfigs/hyprkeybinds.conf ;;
-   " Aussehen und Handhabung") codium ~/.config/hypr/hyprconfigs/hyprlookandfeel.conf ;;
-   "󰍹 Bildschirme") codium ~/.config/hypr/hyprconfigs/hyprmonitors.conf ;;
-   " Berechtigungen") codium ~/.config/hypr/hyprconfigs/hyprpermissions.conf ;;
-   " Programme") codium ~/.config/hypr/hyprconfigs/hyprprograms.conf ;;
-   " Plugins") codium ~/.config/hypr/hyprconfigs/hyprplugins.conf ;;
-   " Fenster und Arbeitsflächen") codium ~/.config/hypr/hyprconfigs/hyprwindowsandworkspaces.conf ;;
-   "󰘇 Dekorationen") codium ~/.config/hypr/hyprconfigs/hyprdecoration.conf ;;
-   "󰥛 Animationen") codium ~/.config/hypr/hyprconfigs/hypranimations.conf ;;
+   "󱓞 Autostart") codium ~/.config/hypr/hyprconfigs/autostart.conf ;;
+   "󰪫 Umgebungsvariablen") codium ~/.config/hypr/hyprconfigs/environment.conf ;;
+   "󰍽 Eingabe") codium ~/.config/hypr/hyprconfigs/input.conf ;;
+   "󰌌 Kurzbefehle") codium ~/.config/hypr/hyprconfigs/keybinds.conf ;;
+   " Aussehen und Handhabung") codium ~/.config/hypr/hyprconfigs/lookandfeel.conf ;;
+   "󰍹 Bildschirme") codium ~/.config/hypr/hyprconfigs/monitors.conf ;;
+   " Berechtigungen") codium ~/.config/hypr/hyprconfigs/permissions.conf ;;
+   " Programme") codium ~/.config/hypr/hyprconfigs/programs.conf ;;
+   " Plugins") codium ~/.config/hypr/hyprconfigs/plugins.conf ;;
+   " Fenster und Arbeitsflächen") codium ~/.config/hypr/hyprconfigs/windowsandworkspaces.conf ;;
+   "󰘇 Dekorationen") codium ~/.config/hypr/hyprconfigs/decorations.conf ;;
+   "󰥛 Animationen") codium ~/.config/hypr/hyprconfigs/animations.conf ;;
    *) exit 1 ;;
 esac
