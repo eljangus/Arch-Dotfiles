768fe44fc5385d828aa20fbeb5430d6ef27e13b8
eljangus <eljangus@gmail.com>
2026-01-28 21:09:03 +0100

    Wallpaper directory name change

--- InstallationScripts/symlinks.sh
+++ InstallationScripts/symlinks.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ln -sf ~/Walls/Walls ~/.config/RofiScripts/WallpaperChanger/Walls
+ln -sf ~/Wallpapers/Walls ~/.config/RofiScripts/WallpaperChanger/Walls
 setsid ~/.config/RofiScripts/Animations/Horizontal/horizontal.sh
 setsid ~/.config/RofiScripts/Dark-Light-Mode/Dunkel/dunkel.sh
 setsid ~/.config/RofiScripts/Rounding/RoundingThemes/20px/round.sh
