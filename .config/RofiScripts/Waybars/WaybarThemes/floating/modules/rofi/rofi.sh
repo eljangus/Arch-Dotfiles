a7b80ce23f4a35ed0a3f85ca0d7b7723e718fc59
eljangus <eljangus@gmail.com>
2026-01-29 18:35:08 +0100

    added slight borders on everything

--- .config/RofiScripts/Waybars/WaybarThemes/floating/modules/rofi/rofi.sh
+++ .config/RofiScripts/Waybars/WaybarThemes/floating/modules/rofi/rofi.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-rofi -show drun -c ~/.config/RofiScripts/Waybars/WaybarThemes/bar/modules/rofi/config.rasi -theme ~/.config/RofiScripts/Waybars/WaybarThemes/bar/modules/rofi/theme.rasi -location 1
+rofi -show drun -c ~/.config/RofiScripts/Waybars/WaybarThemes/floating/modules/rofi/config.rasi -theme ~/.config/RofiScripts/Waybars/WaybarThemes/floating/modules/rofi/theme.rasi -location 1
