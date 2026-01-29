c16f1b770a3d8f2d2bf7673b32717a293c120763
eljangus <eljangus@gmail.com>
2026-01-29 20:46:17 +0100

    added catppuccin color

--- .config/RofiScripts/ColorScheme/scheme.sh
+++ .config/RofiScripts/ColorScheme/scheme.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
 
-chosen=$(printf "Matugen\nEverforest\n" | rofi -dmenu -i -config '~/.config/RofiScripts/Dark-Light-Mode/DL.rasi')
+chosen=$(printf "Matugen\nCatppuccin Mocha Mauve\nEverforest\n" | rofi -dmenu -i -config '~/.config/RofiScripts/ColorScheme/S.rasi')
 
 case "$chosen" in
    "Matugen") ~/.config/RofiScripts/ColorScheme/matugen/matugen.sh && ~/.config/nwg-dock-hyprland/launch.sh ;;
+   "Catppuccin Mocha Mauve") ~/.config/RofiScripts/ColorScheme/Catppuccin-Mocha-Mauve/catppucin-mocha-mauve.sh && ~/.config/nwg-dock-hyprland/launch.sh ;;
    "Everforest") ~/.config/RofiScripts/ColorScheme/Everforest/everforest.sh && ~/.config/nwg-dock-hyprland/launch.sh ;;
    *) exit 1 ;;
 esac
