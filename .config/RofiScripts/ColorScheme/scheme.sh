9a17db4ea40a0ce30533cb0b3364393d3747a5e4
eljangus <eljangus@gmail.com>
2026-01-30 17:06:56 +0100

    added Nord color scheme

--- .config/RofiScripts/ColorScheme/scheme.sh
+++ .config/RofiScripts/ColorScheme/scheme.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
 
-chosen=$(printf "Matugen\nCatppuccin Mocha Mauve\nEverforest\n" | rofi -dmenu -i -config '~/.config/RofiScripts/ColorScheme/S.rasi')
+chosen=$(printf "Matugen\nCatppuccin Mocha Mauve\nEverforest\nNord" | rofi -dmenu -i -config '~/.config/RofiScripts/ColorScheme/S.rasi')
 
 case "$chosen" in
    "Matugen") ~/.config/RofiScripts/ColorScheme/matugen/matugen.sh && ~/.config/nwg-dock-hyprland/launch.sh ;;
    "Catppuccin Mocha Mauve") ~/.config/RofiScripts/ColorScheme/Catppuccin-Mocha-Mauve/catppucin-mocha-mauve.sh && ~/.config/nwg-dock-hyprland/launch.sh ;;
    "Everforest") ~/.config/RofiScripts/ColorScheme/Everforest/everforest.sh && ~/.config/nwg-dock-hyprland/launch.sh ;;
+   "Nord") ~/.config/RofiScripts/ColorScheme/Nord/nord.sh && ~/.config/nwg-dock-hyprland/launch.sh ;;
    *) exit 1 ;;
 esac
