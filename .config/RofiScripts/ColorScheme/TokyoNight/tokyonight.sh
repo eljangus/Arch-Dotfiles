0e41610676dbfcb03c5f32a88fe4b047876032f2
eljangus <eljangus@gmail.com>
2026-01-31 19:37:52 +0100

    added toyko night colorscheme

---
+++ .config/RofiScripts/ColorScheme/TokyoNight/tokyonight.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+
+ln -sf ~/.config/RofiScripts/ColorScheme/matugen/disabled.toml ~/.config/matugen/config.toml
+cp ~/.config/RofiScripts/ColorScheme/TokyoNight/gtk-colors.css ~/.config/gtk-3.0/colors.css
+cp ~/.config/RofiScripts/ColorScheme/TokyoNight/gtk-colors.css ~/.config/gtk-4.0/colors.css
+cp ~/.config/RofiScripts/ColorScheme/TokyoNight/colors.css ~/.config/colors/colors.css
+cp ~/.config/RofiScripts/ColorScheme/TokyoNight/colors.css ~/.config/colors/colors.css
+cp ~/.config/RofiScripts/ColorScheme/TokyoNight/Matugen.conf ~/.config/kitty/themes/Matugen.conf
+cp ~/.config/RofiScripts/ColorScheme/TokyoNight/colors.json ~/.cache/wal/colors.json
+cp ~/.config/RofiScripts/ColorScheme/TokyoNight/colors.rasi ~/.config/colors/colors.rasi
+cp ~/.config/RofiScripts/ColorScheme/TokyoNight/hyprcolors.conf ~/.config/colors/hyprcolors.conf
+cp ~/.config/RofiScripts/ColorScheme/TokyoNight/matugen ~/.config/cava/themes/matugen
+cp ~/.config/RofiScripts/ColorScheme/TokyoNight/matugen.conf ~/.config/qt5ct/colors/matugen.conf
+cp ~/.config/RofiScripts/ColorScheme/TokyoNight/matugen.conf ~/.config/qt6ct/colors/matugen.conf
+cp ~/.config/RofiScripts/ColorScheme/TokyoNight/matugen.theme ~/.config/btop/themes/matugen.theme
+~/.config/RofiScripts/Dark-Light-Mode/dark/dark.sh
